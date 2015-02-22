a845c92c09227bb5d4c3b907fbb0bc089b4a0970
Efimov Artem <efimov.artem@gmail.com>
2015-02-22 11:40:04 +0300

    ...

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
 ## About
 This repo contains course project of 'Getting and Cleaning Data' course provided by Coursera.org and JHBSPH.
 The purpose of this course project is demonstration the ability to collect, work with, and clean a data set. The goal is to prepare tidy data that can be used for later analysis.
-The input data are data collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained: 
-http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+The input data are data collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained:
+
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
 The input data for project are here:
+
 https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
 
 ## Preparing input data
 The input data for project shoud be exracted into the working directory so the directory 'UCI HAR Dataset' should be in the working directory. 
 
 ## How the script run_analysis.R works
 
-1. These data 
+1. Read labels of activitis.
+2. Read activities from 'train' and 'test' sets.
+3. Bind activities into the single data frame, transform activities from data frame to factor so the activites would have descriptive names.
+4. Read subjects from 'train' and 'test' sets.
+5. Bind subjects into the single data frame.
+6. Read features.
+7. Read measured values of features from 'train' and 'test' sets.
+8. Bind sets into the single data frame, assign names of variables from features names.
+9. Extracts indices of the measurements on the mean and standard deviation for each measurement.
+10. Extracts the measurements on the mean and standard deviation for each measurement.
+11. Add to Set data frame columns with ids of subjects and activities.
+12. Create a second, independent tidy data set with the average of each variable for each activity and each subject.
+13. Rename the columns with variables in the second data set to descript that they are average values.
+14. Write tidy data set to file.
--- run_analysis.R
+++ run_analysis.R
@@ -1,36 +1,56 @@
+# 1. Read labels of activitis.
 activity_labels <- read.table("UCI HAR Dataset\\activity_labels.txt", col.names=c("activity_id","activity_label"))
 
+# 2. Read activities from 'train' and 'test' sets.
 activities1 <- read.table('UCI HAR Dataset\\train\\y_train.txt', col.names="activity_id")
 activities2 <- read.table('UCI HAR Dataset\\test\\y_test.txt', col.names="activity_id")
-activities <- rbind(activities1, activities2)
-rm('activities1','activities2')
-fActivities <- factor(activities$activity_id, labels=activity_labels[,2])
 
+# 3. Bind activities into the single data frame, transform activities from data frame to factor
+# so the activites would have descriptive names.
+activities <- rbind(activities1, activities2)
+fActivities <- factor(activities$activity_id, labels=activity_labels[,2])
+rm('activities1','activities2')
+
+# 4. Read subjects from 'train' and 'test' sets.
 subjects1 <- read.table('UCI HAR Dataset\\train\\subject_train.txt', col.names=c('person_id'))
 subjects2 <- read.table('UCI HAR Dataset\\test\\subject_test.txt', col.names=c('person_id'))
+
+# 5. Bind subjects into the single data frame.
 subjects <- rbind(subjects1, subjects2)
 rm('subjects1','subjects2')
 
+# 6. Read features.
 features <- read.table('UCI HAR Dataset\\features.txt', colClasses=c('numeric','character'), nrows=561, col.names=c("feature_id","feature_name"))
 
+# 7. Read measured values of features from 'train' and 'test' sets.
 set1 <- read.table('UCI HAR Dataset\\train\\X_train.txt', colClasses=rep('numeric',561) )
 set2 <- read.table('UCI HAR Dataset\\test\\X_test.txt', colClasses=rep('numeric',561) )
+
+# 8. Bind sets into the single data frame, assign names of variables from features names.
 Set <- rbind(set1, set2)
 names(Set) <- features$feature_name
 rm('set1','set2')
 
+# 9. Extracts indices of the measurements on the mean and standard deviation for each measurement.
 extract_indices <- rbind( grep("min",names(Set)), grep("std",names(Set) ) )
 extract_indices <- sort(as.vector( extract_indices ))
 
+# 10. Extracts the measurements on the mean and standard deviation for each measurement.
 Set <- Set[,extract_indices]
+
+# 11. Add to Set data frame columns with ids of subjects and activities.
 Set <- cbind(subjects, fActivities, Set)
 names(Set)[2] <- "activity"
 
-# Create a second, independent tidy data set with the average of each variable for each activity and each subject.
+# 12. Create a second, independent tidy data set with the average of each variable for each activity and each subject.
+
 SetMelt <- melt(Set, id=c("person_id","activity"), measure.vars = names(Set)[3:length(names(Set))])
 averageSet <- dcast(SetMelt, ...~variable,mean)
+
+# 13. Rename the columns with variables in the second data set to descript that they are average values.
 for(i in 3:length(names(averageSet))) {
     names(averageSet)[i] <- paste("mean(",names(averageSet)[i],")", sep="")
 }
-# Write to file for upload.
+
+# 14. Write tidy data set to file.
 write.table(averageSet, file = "averageSet.txt", row.name=FALSE)
